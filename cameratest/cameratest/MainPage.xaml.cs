ea6e3e519872e07424b7024a2fc80ae97c384591
coldstarjoey <joey@coldstarsystems.co.uk>
2021-03-24 10:02:04 +0000

    init push

---
+++ cameratest/cameratest/MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace cameratest
+{
+    public partial class MainPage : ContentPage
+    {
+        public static String PhotoPath { get; set; }
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await TakePhotoAsync();
+        }
+
+        async Task TakePhotoAsync()
+        {
+            try
+            {
+                var photo = await MediaPicker.CapturePhotoAsync();
+                await LoadPhotoAsync(photo);
+                Console.WriteLine($"CapturePhotoAsync COMPLETED: {PhotoPath}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"CapturePhotoAsync THREW: {ex.Message}");
+            }
+        }
+
+        async Task LoadPhotoAsync(FileResult photo)
+        {
+            // canceled
+            if (photo == null)
+            {
+                PhotoPath = null;
+                return;
+            }
+            // save the file into local storage
+            var newFile = Path.Combine(FileSystem.CacheDirectory, photo.FileName);
+            using (var stream = await photo.OpenReadAsync())
+            using (var newStream = File.OpenWrite(newFile))
+                await stream.CopyToAsync(newStream);
+
+            PhotoPath = newFile;
+
+            byte[] result = System.Text.Encoding.ASCII.GetBytes(PhotoPath);
+
+            Stream stream1 = new MemoryStream(result);
+            var imageSource = ImageSource.FromStream(() => stream1);
+
+            MyImage.Source = imageSource;
+        }
+    }
+}
